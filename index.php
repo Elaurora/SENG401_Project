dc5f0fb2a9d209e140a33c61d59d82a2b181dd26
psettle <patricksettle@hotmail.com>
2017-03-07 10:09:07 -0700

    Skeleton

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
 <?php
-echo 'hi';
+
+
+
+
+define('__LOAD_BALANCER__', false);
+define('__CONFIG_GUI__', false);
+define('__NODE_SERVER__', true);
+
+
+/**
+ * Starting point for the node server
+ */
+function node() {
+	
+}
+
+/**
+ * Starting point for the config GUI
+ */
+function config() {
+	
+}
+
+/**
+ * Starting point for the load balancer
+ */
+function load() {
+	
+}
+
+
+/**
+ * Autoloader for dependencies
+ */
+function import() {
+	
+}
+
+import();
+if(__LOAD_BALANCER__) {
+	load();
+} else if(__CONFIG_GUI__) {
+	config();
+} else if(__NODE_SERVER__) {
+	node();
+}
