2403066a072533ff47619e930d2a7e9bdb1edf60
Andy <andyschndr@gmail.com>
2017-03-13 13:37:04 -0600

    Request executor and retriever added, but not tested yet

---
+++ RetrievalServices/RESTDataRetriever.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Andy
+ * responsible for retrieving and returning JSON objects
+ * sent back from URL requests specified by the RequestExecutor.
+ */
+class RequestDataRetriever {
+	
+	/**
+	 * Completes the passed URL request and returns
+	 * the JSON object retrieved.
+	 * @param string $url
+	 * 		The request url that was sent to be completed.
+	 * @return
+	 */
+	public function completeRequest($url) {
+		
+		$json = file_get_contents($url);
+		return json_decode($json);
+		
+	}
+}
