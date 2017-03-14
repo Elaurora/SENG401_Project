94902a307123f2fdc619d8dab2cd2cf2e663adc2
Andy <andyschndr@gmail.com>
2017-03-14 09:13:30 -0600

    added the controller. it does jack shite currently

---
+++ RequestServices/RequestController/RequestController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+
+class RequestController {
+
+	/**
+	 * Completes the passed URL request and returns
+	 * the JSON object retrieved.
+	 * @param string $url
+	 * 		The request url that was sent to be completed.
+	 * @return
+	 */
+	public function completeRequest($url) {
+
+		$json = file_get_contents($url);
+		return $json;
+		//to handle receiving images, no longer do json stuff with response
+		//return json_decode($json);
+
+	}
+}
