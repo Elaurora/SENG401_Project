473d9eee57c292c8ec007a82381b143126b12d03
psettle <patricksettle@hotmail.com>
2017-03-30 11:15:01 -0600

    Updated Schema

---
+++ vendor/bin/CacheMatchVariableQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+use Base\CacheMatchVariableQuery as BaseCacheMatchVariableQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'cache_match_variables' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class CacheMatchVariableQuery extends BaseCacheMatchVariableQuery
+{
+
+}
---
+++ vendor/bin/CachedRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Base\CachedRequest as BaseCachedRequest;
+
+/**
+ * Skeleton subclass for representing a row from the 'cached_requests' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class CachedRequest extends BaseCachedRequest
+{
+
+}
