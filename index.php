7b3a4f0d253fe27dce7381524db9b420b39c7f18
psettle <patricksettle@hotmail.com>
2017-03-30 14:55:51 -0600

    Updated autoloader

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-use Base\CachedRequests;
-use Propel\Runtime\Connection\ConnectionFactory;
-
-/**
- * Autoloader for dependencies, put all the files you create in this list
- */
-function import() {
-	include_once 'vendor/autoload.php';
-	require_once 'vendor/bin/generated-conf/config.php';
-	include_once 'RequestServices/RequestController/RequestController.php';
-    include_once 'RequestServices/RESTRequestHandler/RESTRequestHandler.php';
-    include_once 'RequestServices/RequestExecutor/RequestExecutor.php';
-	include_once 'RequestServices/RequestTranslation/RequestBuilder/RequestBuilder.php';
-	include_once 'RequestServices/RequestTranslation/RequestParser/RequestParser.php';
-	include_once 'RequestServices/RequestTranslation/Request.php';
-    include_once 'RetrievalServices/RESTDataRetriever.php';
-}
 
 define('__GLOBAL_DATABASE__', true);
 define('__CONFIG_GUI__', false);
@@ -42,71 +25,13 @@ function config() {
 }
 
 /**
- * Starting point for the load balancer
+ * Starting point for the global database webserver
  */
 function global_db() {
-	/*
-	 * DB Schema
-	 * 
-	 * TABLE: cached_requests
-	 * query_id : int
-	 * query_url : string
-	 * query_response : binary dump
-	 * PrimaryKey: {query_id}
-	 * 
-	 * 
-	 * TABLE: get_variables
-	 * query_id : int (foreign key on cached_requests.query_id)
-	 * variable_name : string
-	 * variable_value: string
-	 * PrimaryKey: {query_id, variable_name}
-	 * 
-	 * TABLE: cache_rules
-	 * rule_id : int
-	 * local_ttl : int (seconds)
-	 * global_ttl : int (seconds)
-	 * PrimaryKey: {rule_id}
-	 * 
-	 * TABLE: cache_match_variables
-	 * rule_id : int (foreign key on cache_rules.rule_id)
-	 * variable_name : string
-	 * variable_value: string
-	 * PrimaryKey : {rule_id, variable_name, variable_value}
-	 * 
-	 */
-	
-	
-		$request = new \CachedRequests();
-		
-		$request->setQueryResponse("Hello World!");
-		$request->setQueryTime(300);
-		$request->save();
-		
-	
-		$query = CachedRequestsQuery::create();
-		$query = $query->filterByQueryResponse("Hello World!");
-		$query = $query->findOne();
-		
-		//$query->delete();
-		
-		//$request = new \GlobalCachedRequests();
-		
-		//$request->setQueryResponse("Hello World!");
-		//$request->save();
-		
-		
-		//$query = GlobalCachedRequestsQuery::create();
-		//$query = $query->filterByQueryResponse("Hello World!");
-		//$query = $query->findOne();
-				
-		//$query->delete();
-	
-	
-
-	
+	node();
 }
 
-import();
+
 if(__GLOBAL_DATABASE__) {
 	global_db();
 } else if(__CONFIG_GUI__) {
