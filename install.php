4f27484ab583e39ef48a3bb296cb08081fbe7527
psettle <patricksettle@hotmail.com>
2017-04-08 21:06:51 -0600

    HAAAAANNNDDS

--- install.php
+++ install.php
@@ -8,14 +8,26 @@ $globalDBIP = '104.198.102.12';
 
 $subscribeResults = file_get_contents('http://' . $globalDBIP . '/?type=subscribe');
 
-echo 'Got rule results: <br><pre>';
-echo $subscribeResults;
-echo '</pre>';
+
 
 $subscribeResults = json_decode($subscribeResults);
 
+echo 'Got rule results: <br><pre>';
+echo print_r($subscribeResults);
+echo '</pre>';
+
 $rules = $subscribeResults->rules;
 $variables = $subscribeResults->variables;
+
+echo 'Got rules: <br><pre>';
+echo print_r($rules);
+echo '</pre>';
+
+echo 'Got variables: <br><pre>';
+echo print_r($variables);
+echo '</pre>';
+
+echo '<br>Parsing Rules...<br>';
 
 $rulesToSave = array();
 
@@ -28,6 +40,7 @@ foreach($rules as $rule) {
 	$rulesToSave[$rule['RuleId']] = $toSave;
 }
 echo '<br>Rules Parsed...<br>';
+echo '<br>Parsing Variables...<br>';
 //iterate over each variable and add it to the rule
 foreach($variables as $variable) {
 	$toSave = new CacheMatchVariable();
@@ -37,6 +50,7 @@ foreach($variables as $variable) {
 	$rulesToSave[$variable['RuleId']]->addCacheMatchVariable($toSave);
 }
 echo '<br>Variables Parsed...<br>';
+echo '<br>Saving Rules...<br>';
 //iterate over each rule and save
 foreach($rulesToSave as $ruleToSave) {
 	$rulesToSave->save();
